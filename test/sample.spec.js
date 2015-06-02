b6ae7671729a6b816c870497a1f1ae0ca31f7324
scott coates <scoarescoare@gmail.com>
2015-06-01 20:21:02 -0500

    Reference using the jsx loader.

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -2,7 +2,7 @@
 'use strict';
 
 import React from 'react';
-import Feed from '../build/js/components/Feed';
+import Feed from '../build/js/components/Feed.js!jsx';
 
 describe('A test suite', function() {
 
