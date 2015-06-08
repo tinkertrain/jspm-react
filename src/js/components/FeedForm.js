fc048e7e10cfcda3a85910a2ede4bb7ca1ae5517
Marques Woodson <marques.woodson@uptake.com>
2015-06-07 19:10:58 -0500

    Updating React to latest version, and switching to es6 classes for each component.

--- src/js/components/FeedForm.js
+++ src/js/components/FeedForm.js
@@ -1,20 +1,18 @@
 /* jshint -W030 */
 'use strict';
-
 import React from 'react';
-
-var FeedForm = React.createClass({
-  render: function() {
+var {Component} = React;
+class FeedForm extends Component {
+  render() {
     return (
-      <form className="container">
-        <div className="form-group">
-          <input type="text" className="form-control" placeholder="Titles" />
-          <input type="text" className="form-control" placeholder="Description" />
-          <button className="btn btn-primary btn-block" type="submit">Add</button>
-        </div>
-      </form>
+        <form className="container">
+          <div className="form-group">
+            <input type="text" className="form-control" placeholder="Titles" />
+            <input type="text" className="form-control" placeholder="Description" />
+            <button className="btn btn-primary btn-block" type="submit">Add</button>
+          </div>
+        </form>
     );
   }
-});
-
+}
 export default FeedForm;
